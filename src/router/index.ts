ae15f85f391d2ea4f57794bea2f5da7e51e453d8
Damis Bachtiar <damis.bachtiar@gmail.com>
2024-07-01 14:38:53 +0700

    feat: add menu and dashboard component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,18 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import HomePage from '../views/HomePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
+    path: '',
     redirect: '/home'
   },
   {
     path: '/home',
-    name: 'Home',
-    component: HomePage
+    component: () => import ('../views/HomePage.vue')
+  },
+  {
+    path: '/folder/:id',
+    component: () => import ('../views/FolderPage.vue')
   }
 ]
 
